dc62643240c354febb514b8783f3ee475f4c6cda
studycode167 <77335708+studycode167@users.noreply.github.com>
2021-11-14 13:52:28 +0900

    [추가]홈 화면 구현

--- TasteAlarm/app/src/main/java/org/techtown/tastealarm/HomeActivity.java
+++ TasteAlarm/app/src/main/java/org/techtown/tastealarm/HomeActivity.java
@@ -3,6 +3,7 @@ package org.techtown.tastealarm;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,4 +14,5 @@ public class HomeActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
 }
