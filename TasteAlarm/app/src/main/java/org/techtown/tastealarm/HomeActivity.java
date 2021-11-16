691e861c961891496e5def81a89d253a1ca27e02
iamwotjr99 <iamwotjr@gmail.com>
2021-11-16 23:07:13 +0900

    [추가] 네이버지도 현재위치 ui, 현재 위치 기능 자바코드 구현

--- TasteAlarm/app/src/main/java/org/techtown/tastealarm/HomeActivity.java
+++ TasteAlarm/app/src/main/java/org/techtown/tastealarm/HomeActivity.java
@@ -3,6 +3,7 @@ package org.techtown.tastealarm;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,6 +12,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +28,26 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class HomeActivity extends Fragment {
+public class HomeActivity extends Fragment implements OnMapReadyCallback {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
     private List<Restaurant> mList = new ArrayList<Restaurant>();
     private HomeAdapter adapter;
+    private NaverMap naverMap;
+    private FusedLocationSource locationSource;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.home_naverMap);
+        if(mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.home_naverMap, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(ServerURL.SERVER_URL)
@@ -66,4 +86,23 @@ public class HomeActivity extends Fragment {
         return view;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한이 거부 될 경우
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.getUiSettings().setLocationButtonEnabled(true);
+    }
 }
